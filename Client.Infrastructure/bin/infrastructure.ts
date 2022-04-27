79b9dedaff6d53ad0b348615a134fdf442ea37cc
Phil Merrell <philmerrell@boisestate.edu>
2022-04-26 22:13:20 -0600

    Add cognito stack

--- Client.Infrastructure/bin/infrastructure.ts
+++ Client.Infrastructure/bin/infrastructure.ts
@@ -2,8 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ClientStack } from '../lib/client-stack';
+import { CognitoStack } from '../lib/cognito-stack';
 
 const app = new cdk.App();
 new ClientStack(app, 'SubmissionHoldClientStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
+
+new CognitoStack(app, 'SubmissionHoldCognitoStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
