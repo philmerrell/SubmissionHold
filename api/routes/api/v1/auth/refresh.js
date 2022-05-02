470eebe588836a3be7e568e90142e36a9d8d0c2d
Phil Merrell <philmerrell@boisestate.edu>
2022-05-02 09:38:21 -0600

    fix refresh token endpoint

--- api/routes/api/v1/auth/refresh.js
+++ api/routes/api/v1/auth/refresh.js
@@ -1,13 +1,21 @@
 const Router = require('express').Router
-const { cognito_client_id, cognito_base_url, cognito_logout_uri} = require('../../../../config');
+const { cognito_client_id, cognito_base_url } = require('../../../../config');
 
 
 module.exports = Router({ mergeParams: true })
-  .get('/v1/auth/logout', async (req, res, next) => {
+  .post('/v1/auth/refresh', async (req, res, next) => {
+    const refresh_token = req.body.refresh_token;
     try {
-      const redirectUrl = encodeURIComponent(cognito_logout_uri);
-      const logoutUrl = `${cognito_base_url}/logout?client_id=${cognito_client_id}&logout_uri=${redirectUrl}`;
-      res.status(301).redirect(logoutUrl);
+      const response = await axios({
+        method: 'POST',
+        url: `${cognito_base_url}/oauth2/token`,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: 'Basic ' + Buffer.from(`${cognito_client_id}:${cognito_client_secret}`).toString('base64')
+        },
+        data: { grant_type: 'refresh_token', client_id: cognito_client_id, refresh_token }
+      });
+      res.json(response.data);
     } catch (error) {
       next(error)
     }
