9dd799c3a54f877ad1cbf8670c2eb6493b73abf2
Phil Merrell <philmerrell@boisestate.edu>
2022-05-27 11:54:02 -0600

    Add login buton on logout view

--- client/src/app/logout/logout.page.ts
+++ client/src/app/logout/logout.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutPage implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authService.login();
   }
 
 }
