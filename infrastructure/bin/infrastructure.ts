1f0e815a8062ade045357e994dcbf5ea1add0b91
Phil Merrell <philmerrell@boisestate.edu>
2022-05-26 22:08:21 -0600

    update infrastructure tags

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -4,16 +4,22 @@ import * as cdk from 'aws-cdk-lib';
 import { ClientStack } from '../lib/client-stack';
 import { CognitoStack } from '../lib/cognito-stack';
 import { ApiStack } from '../lib/api-stack';
+import { Tags } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-new ClientStack(app, 'SubmitClientStack', {
+const clientStack = new ClientStack(app, 'SubmitClientStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
+Tags.of(clientStack).add('Billing', 'Treefort');
 
-new CognitoStack(app, 'SubmitCognitoStack', {
+
+const cognitoStack = new CognitoStack(app, 'SubmitCognitoStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
+Tags.of(cognitoStack).add('Billing', 'Treefort');
 
-new ApiStack(app, 'SubmitApiStack', {
+
+const apiStack = new ApiStack(app, 'SubmitApiStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
+Tags.of(apiStack).add('Billing', 'Treefort');
