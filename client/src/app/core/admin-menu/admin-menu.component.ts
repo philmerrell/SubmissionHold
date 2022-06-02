739a0e7b451f1d260ccec5f76a17f6616dc63266
Phil Merrell <philmerrell@boisestate.edu>
2022-06-01 21:44:30 -0600

    Display role based side nav options

--- client/src/app/core/admin-menu/admin-menu.component.ts
+++ client/src/app/core/admin-menu/admin-menu.component.ts
@@ -10,14 +10,6 @@ import { User } from '../../auth/user.service';
 export class AdminMenuComponent implements OnInit {
   @Input() user: User;
 
-  public appPages = [
-    { title: 'Inbox', url: '/admin/submissions', icon: 'folder' },
-    { title: 'My Votes', url: '/admin/my-votes', icon: 'ticket' },
-    { title: 'Ignored', url: '/admin/ignored', icon: 'skull' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Users', url: '/admin/users', icon: 'people' },
-    { title: 'Settings', url: '/admin/settings', icon: 'settings' },
-  ];
   public labels = [];
 
   constructor(private settingService: SettingService) { }
