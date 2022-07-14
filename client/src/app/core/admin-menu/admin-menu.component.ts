bd82437f822166ab67ea42800a3bf128fb77cdb8
Phil Merrell <philmerrell@boisestate.edu>
2022-07-13 22:59:39 -0600

    Create labels from sidenav

--- client/src/app/core/admin-menu/admin-menu.component.ts
+++ client/src/app/core/admin-menu/admin-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { Festival } from '../../admin/services/admin-festival.service';
 import { LabelService, LabelsResponse } from '../../admin/services/label.service';
 import { User } from '../../auth/user.service';
@@ -15,16 +16,55 @@ export class AdminMenuComponent implements OnInit {
   labelResponse: LabelsResponse;
   labelsRequestComplete: boolean;
 
-  constructor(private festivalService: ActiveFestivalService, private labelService: LabelService) { }
+  constructor(private alertController: AlertController, private festivalService: ActiveFestivalService, private labelService: LabelService) { }
 
   async ngOnInit() {
+    console.log(this.user);
     this.activeFestival = await this.festivalService.getActiveFestival();
     this.getLabels();
   }
 
+  async createLabel(name: string) {
+    await this.labelService.createLabel(this.activeFestival.id, { name, submissionIds: []});
+    this.getLabels();
+  }
+
   async getLabels() {
+    this.labelsRequestComplete = false;
     this.labelResponse = await this.labelService.getLabels(this.activeFestival.id);
     this.labelsRequestComplete = true;
   }
+
+  async presentCreateLabelAlert() {
+    const alert = await this.alertController.create({
+      header: 'Create a Label',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Enter a label name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'OK',
+          role: 'confirm'
+        }
+      ]
+    });
+
+    await alert.present();
+
+    const { role, data } = await alert.onDidDismiss();
+    console.log(data);
+    if (role === 'confirm' && data.values.name !== '') {
+      this.createLabel(data.values.name);
+    }
+  }
+
+
 
 }
