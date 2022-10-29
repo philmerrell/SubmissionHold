e66308ce47a8c08d3f55e7746ab0ba28eea11df6
Phil Merrell <philmerrell@boisestate.edu>
2022-10-29 10:33:05 -0600

    Add loading indicator

---
+++ client/src/app/admin/pages/vote-tally/submissions-loading/submissions-loading.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submissions-loading',
+  templateUrl: './submissions-loading.component.html',
+  styleUrls: ['./submissions-loading.component.scss'],
+})
+export class SubmissionsLoadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- client/src/app/admin/pages/vote-tally/vote-tally.module.ts
+++ client/src/app/admin/pages/vote-tally/vote-tally.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { VoteTallyPageRoutingModule } from './vote-tally-routing.module';
 
 import { VoteTallyPage } from './vote-tally.page';
+import { SubmissionsLoadingComponent } from './submissions-loading/submissions-loading.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { VoteTallyPage } from './vote-tally.page';
     IonicModule,
     VoteTallyPageRoutingModule
   ],
-  declarations: [VoteTallyPage]
+  declarations: [VoteTallyPage, SubmissionsLoadingComponent]
 })
 export class VoteTallyPageModule {}
